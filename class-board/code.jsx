41006dcfce5ba8bd6e2d171275e255a403d8a5fe
Avi <avicreationstudio@gmail.com>
2024-05-02 15:18:52 +0530

    updated task

--- class-board/code.jsx
+++ class-board/code.jsx
@@ -1,15 +1,14 @@
+// rendering
 function TestingComponent() {
-    let arr = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10];
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     function handleClick(value) {
         alert("btn clicked " + value);
     }
     return (
         <>
-            {
-                arr.map((value) => {
-                    return <button onClick={() => handleClick(value)}>{value}</button>
-                })
-            }
+            <button onClick={() => handleClick(1)}>1</button>
+            <button onClick={() => handleClick(2)}>2</button>
+            <button onClick={() => handleClick(3)}>3</button>
         </>
     );
 }
