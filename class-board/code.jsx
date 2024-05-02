411123ac9f9ec3a09fbb904c018678efd70abd2f
Avi <avicreationstudio@gmail.com>
2024-05-02 18:28:26 +0530

    props

--- class-board/code.jsx
+++ class-board/code.jsx
@@ -1,18 +1,28 @@
-// rendering
-function TestingComponent() {
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    function handleClick(value) {
-        alert("btn clicked " + value);
+// This is Parent Compenent
+
+import { useState } from "react";
+import ChildComponent from "./ChildComponent";
+function ParentComponent() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        setCount(count + 1);
     }
-    return (
-        <>
-            {
-                arr.map((value) => {
-                    return <button onClick={() => handleClick(value)}>{value}</button>;
-                })
-            }
-        </>
-    );
+    return (<>
+        <button onClick={handleClick}>parent count {count}</button>
+        <ChildComponent value={count} />
+        <ChildComponent value={count} />
+        <br />
+    </>)
 }
 
-export default TestingComponent;
+export default ParentComponent;
+
+
+// ---------------------------------------------------------------------------------
+// This is child component
+
+function ChildComponent(props) {
+    return <button>child count {props.value}</button>;
+}
+
+export default ChildComponent;
