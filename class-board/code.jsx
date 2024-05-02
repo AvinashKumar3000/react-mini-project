ed80eaa98dd4e686b4696702fa8a59eed3f8eed7
Avi <avicreationstudio@gmail.com>
2024-05-02 11:43:31 +0530

    task 01

--- class-board/code.jsx
+++ class-board/code.jsx
@@ -1,41 +1,15 @@
-// rendering list with components
-
-function SampleComponent() {
-    return <h1>sample value</h1>;
-}
 function TestingComponent() {
-    let arr = [1, 2, 3, 4];
-    return (
-        <ul>
-            {
-                arr.map((value) => {
-                    return <SampleComponent />;
-                })
-            }
-        </ul>
-    );
-}
-
-export default TestingComponent;
-
-
-
-function TestingComponent() {
-    // normal function
+    let arr = [1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10];
     function handleClick(value) {
-        alert("button clicked " + value);
+        alert("btn clicked " + value);
     }
-    // function expression
-    // let handleClick = function () {
-    //     alert("button clicked ");
-    // }
     return (
         <>
-            <button onClick={() => {
-                alert("arrow function");
-            }}> 1 </button>
-            <button onClick={() => handleClick(2)}> 2 </button>
-            <button onClick={() => handleClick(3)}> 3 </button>
+            {
+                arr.map((value) => {
+                    return <button onClick={() => handleClick(value)}>{value}</button>
+                })
+            }
         </>
     );
 }
