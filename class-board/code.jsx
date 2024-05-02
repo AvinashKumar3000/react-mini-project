57495ad5df0a9a0a5e29a39e366bbadcf0211efe
Avi <avicreationstudio@gmail.com>
2024-05-02 10:41:33 +0530

    rendering list

--- class-board/code.jsx
+++ class-board/code.jsx
@@ -1,1 +1,19 @@
-// class code
+// rendering list with components
+
+function SampleComponent() {
+    return <h1>sample value</h1>;
+}
+function TestingComponent() {
+    let arr = [1, 2, 3, 4];
+    return (
+        <ul>
+            {
+                arr.map((value) => {
+                    return <SampleComponent />;
+                })
+            }
+        </ul>
+    );
+}
+
+export default TestingComponent;
