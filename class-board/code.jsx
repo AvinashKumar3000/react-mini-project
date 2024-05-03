40de6daa8c52e79a01d940357980e8cb5c678d3e
Avi <avicreationstudio@gmail.com>
2024-05-03 15:43:39 +0530

    updated data.js

--- class-board/code.jsx
+++ class-board/code.jsx
@@ -32,3 +32,33 @@ function Card({ obj }) {
 }
 
 export default Card;
+
+// data.js
+const NARUTO_ANIME = [
+    {
+        name: "Naruto Uzumaki",
+        power: "Rasengan",
+        strength_level: "High",
+        hair_color: "Blond"
+    },
+    {
+        name: "Sasuke Uchiha",
+        power: "Chidori",
+        strength_level: "High",
+        hair_color: "Black"
+    },
+    {
+        name: "Sakura Haruno",
+        power: "Medical Ninjutsu",
+        strength_level: "Medium",
+        hair_color: "Pink"
+    },
+    {
+        name: "Kakashi Hatake",
+        power: "Sharingan",
+        strength_level: "High",
+        hair_color: "White"
+    }
+];
+
+export { NARUTO_ANIME };
