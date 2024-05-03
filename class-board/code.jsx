183b436305127e73d00060f16b658d890a106051
Avi <avicreationstudio@gmail.com>
2024-05-03 11:52:43 +0530

    data

--- class-board/code.jsx
+++ class-board/code.jsx
@@ -1,55 +1,26 @@
-// Card.jsx file
-function Card({ title, hero, heroine }) {
-    return (
-        <fieldset>
-            <legend>{title}</legend>
-            <ul>
-                <li>{hero}</li>
-                <li>{heroine}</li>
-            </ul>
-        </fieldset>
-    );
-}
-
-export default Card;
-
-// ParentComponent.jsx
-import Card from "./Card";
-
-function ParentComponent() {
-
-    const movies = [
-        {
-            title: "Enthiran",
-            hero: "Rajini",
-            heroine: "Aishwarya Rai"
-        },
-        {
-            title: "Leo",
-            hero: "Vijay",
-            heroine: "trisha"
-        },
-        {
-            title: "Mangatha",
-            hero: "Ajith kumar",
-            heroine: "trisha"
-        },
-        {
-            title: "PK",
-            hero: "Amir khan",
-            heroine: "Anushka sharma"
-        }
-
-    ]
-    return (
-        <>
-            {
-                movies.map((value) => {
-                    return <Card title={value.title} hero={value.hero} heroine={value.heroine} />
-                })
-            }
-        </>
-    );
-}
-
-export default ParentComponent;
+const characters = [
+    {
+        name: "Naruto Uzumaki",
+        power: "Rasengan",
+        strength_level: "High",
+        hair_color: "Blond"
+    },
+    {
+        name: "Sasuke Uchiha",
+        power: "Chidori",
+        strength_level: "High",
+        hair_color: "Black"
+    },
+    {
+        name: "Sakura Haruno",
+        power: "Medical Ninjutsu",
+        strength_level: "Medium",
+        hair_color: "Pink"
+    },
+    {
+        name: "Kakashi Hatake",
+        power: "Sharingan",
+        strength_level: "High",
+        hair_color: "White"
+    }
+];
