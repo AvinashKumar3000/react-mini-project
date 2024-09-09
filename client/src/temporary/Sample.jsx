5c7b7fa750a9aed578cc6045424f806d5151c56c
Avi <avicreationstudio@gmail.com>
2024-09-09 16:59:41 +0530

    'a'

--- client/src/temporary/Sample.jsx
+++ client/src/temporary/Sample.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import "./mycss.css"; // 👈 make sure to include correct .css file.
 
+const reactionArray = ["🥲", "😞", "😔", "😟", "☹️", "😊", "😄", "😁", "😀", "😃", "😆", "😂"]
+function Reaction() {
+    const [count, setCount] = useState(0); // 👈 01
+    const [emoji, setEmoji] = useState(reactionArray[0]);
+    function handleClick() {
+        if (count <= reactionArray.length) {
+            setEmoji(reactionArray[count + 1]);
+        } else {
+            setEmoji(reactionArray[reactionArray.length - 2]);
+        }          // 👈 02
+        setCount(count + 1)
+    }
+    return (
+        <div className="container">
+            like : {count}{emoji}
+            <br />
+            <i id="heart" onClick={handleClick} className="fa-solid fa-heart"></i>
+        </div>
+    );
+}
+
+
+// export default App;
 
 function Sample() {
     return (
         <>
+            <Reaction />
         </>
     );
 }
